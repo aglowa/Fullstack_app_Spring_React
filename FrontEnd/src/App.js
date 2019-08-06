f68b5994fc3c9d586fbefd72a0ff76f59c6ebb7a
Adrian <40326096+aglowa@users.noreply.github.com>
2019-08-07 01:32:47 +0200

    Add files via upload

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -2,15 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
-import ProjectBoard from './components/ProjectBoard'
+import ProjectBoard from './components/ProjectBoard';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProjectTask from './components/ProjectTask/AddProjectTask';
 
-function App() {
-  return (
-      <div className="App">
-          <Navbar />
-          <ProjectBoard/>
-    </div>
-  );
+class App extends Component {
+    render(){
+    return (
+        <Router>
+            <div className="App">
+                <Navbar />
+
+                <Route exact path="/" component={ProjectBoard} />
+
+                <Route exact path="/addProjectTask" component={AddProjectTask} />
+            </div>
+        </Router>
+    );
+}
 }
 
 export default App;
